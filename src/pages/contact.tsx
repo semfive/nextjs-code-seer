4b974893dfdd7ad76ed284a095aa615acc950765
Nguyen Kien <ngkien299@gmail.com>
2023-04-14 03:49:53 +0700

    Firt styling Contact Page

---
+++ src/pages/contact.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div className='w-full h-full bg-[#F0F0F0] flex justify-center items-center'>
+      Contact
+    </div>
+  );
+};
+
+export default Contact;
