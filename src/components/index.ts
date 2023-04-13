07c556db2ab15fc12a41399d2d18665e836ff347
Nguyen Kien <ngkien299@gmail.com>
2023-04-14 03:48:59 +0700

    style: This file for export all Components in components folder

---
+++ src/components/index.ts
@@ -0,0 +1,8 @@
+import ButtonOutline from './common/ButtonOutline';
+import PricingCard from './common/PricingCard/PricingCard';
+import ButtonFilled from './common/ButtonFilled';
+import Layout from './Layout/Layout';
+import Footer from './common/Footer/Footer';
+import Navbar from './common/Navbar/Navbar';
+
+export { ButtonOutline, PricingCard, ButtonFilled, Layout, Footer, Navbar };
