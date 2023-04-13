b9db4e02dd2cb750d517d6f2658890b2293214d0
Nguyen Kien <ngkien299@gmail.com>
2023-04-14 03:40:57 +0700

    Create ButtonFilled component

---
+++ src/components/common/ButtonFilled/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+interface IButtonFilled {
+  id?: string;
+  className?: string;
+  onClick?: any;
+  children?: any;
+  text?: string;
+  style?: React.CSSProperties;
+}
+
+const ButtonFilled = ({
+  id,
+  className,
+  onClick,
+  children,
+  text,
+  style,
+}: IButtonFilled) => {
+  return (
+    <button
+      id={id}
+      className={`${className} bg-dark_blue border-2 border-solid border-dark_blue py-2 px-8 font-semibold text-white hover:bg-opacity-95 hover:text-white transition`}
+      style={style}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default ButtonFilled;
