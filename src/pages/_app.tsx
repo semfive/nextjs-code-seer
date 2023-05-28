d71fb52cc2a9f7cac4a3499c45c0ae438146a8c0
Nguyen Kien <ngkien299@gmail.com>
2023-05-28 10:29:13 +0700

    Add google Analytic

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -29,20 +29,6 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
           {getLayout(<Component {...pageProps} />)}
         </Providers>
       </ReactFlowProvider>
-      <Script
-        async={true}
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-        strategy='afterInteractive'
-      />
-      <Script id='google-analytics' strategy='afterInteractive'>
-        {`
-     window.dataLayer = window.dataLayer || [];
-     function gtag(){dataLayer.push(arguments);}
-     gtag('js', new Date());
-    
-      gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
-    `}
-      </Script>
     </>
   );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,25 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
   return (
     <Html lang='en'>
-      <Head />
+      <Head>
+        <Script
+          async={true}
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          strategy='afterInteractive'
+        />
+        <Script id='google-analytics' strategy='afterInteractive'>
+          {`
+     window.dataLayer = window.dataLayer || [];
+     function gtag(){dataLayer.push(arguments);}
+     gtag('js', new Date());
+    
+      gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+    `}
+        </Script>
+      </Head>
       <body>
         <Main />
         <NextScript />
