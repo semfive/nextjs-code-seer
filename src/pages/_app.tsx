1d514dbb5e3a0d0e9eb172e569cf898843f3a523
Nguyen Kien <ngkien299@gmail.com>
2023-05-28 10:37:23 +0700

    add title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,6 +9,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import 'react-quill/dist/quill.snow.css';
 import Providers from '@/components/common/Providers';
 import Script from 'next/script';
+import Head from 'next/head';
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -23,12 +24,34 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
 
   return (
     <>
+      <Head>
+        <title>CodeSeer</title>
+        <meta
+          property='description'
+          content='CodeSeer is a code visualize platform'
+        />
+      </Head>
       <ReactFlowProvider>
         <Providers>
           <ToastContainer />
           {getLayout(<Component {...pageProps} />)}
         </Providers>
       </ReactFlowProvider>
+
+      <Script
+        async={true}
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        strategy='afterInteractive'
+      />
+      <Script id='google-analytics' strategy='afterInteractive'>
+        {`
+     window.dataLayer = window.dataLayer || [];
+     function gtag(){dataLayer.push(arguments);}
+     gtag('js', new Date());
+    
+      gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+    `}
+      </Script>
     </>
   );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,22 +4,7 @@ import Script from 'next/script';
 export default function Document() {
   return (
     <Html lang='en'>
-      <Head>
-        <Script
-          async={true}
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-          strategy='afterInteractive'
-        />
-        <Script id='google-analytics' strategy='afterInteractive'>
-          {`
-     window.dataLayer = window.dataLayer || [];
-     function gtag(){dataLayer.push(arguments);}
-     gtag('js', new Date());
-    
-      gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
-    `}
-        </Script>
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
